c48acc8b71a3aae8c1a837c26661a98ff8a973f4
genialojerem <166065024+genialo555@users.noreply.github.com>
2024-07-30 14:34:28 +0200

    Add files via upload

--- projet studi/config/database.php
+++ projet studi/config/database.php
@@ -1,3 +1,39 @@
 <?php
-$mongoClient = new MongoDB\Client("mongodb://localhost:27017");
-$database = $mongoClient->selectDatabase("zoo_arcadia");
+require 'vendor/autoload.php';
+
+class Database
+{
+    private static $mongoConnection;
+    private static $mysqlConnection;
+
+    public static function getMongoConnection()
+    {
+        if (!self::$mongoConnection) {
+            $client = new MongoDB\Client("mongodb://localhost:27017");
+            self::$mongoConnection = $client->selectDatabase('zoo');
+        }
+        return self::$mongoConnection;
+    }
+
+    public static function getMysqlConnection()
+    {
+        if (!self::$mysqlConnection) {
+            $host = 'localhost';
+            $dbname = 'zoo_arcadia';
+            $username = 'votre_nom_utilisateur';
+            $password = 'votre_mot_de_passe';
+
+            try {
+                self::$mysqlConnection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+                self::$mysqlConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Erreur de connexion à la base de données MySQL : " . $e->getMessage());
+            }
+        }
+        return self::$mysqlConnection;
+    }
+}
+
+// Exemple d'utilisation des connexions
+$mongoDatabase = Database::getMongoConnection();
+$mysqlConnection = Database::getMysqlConnection();
